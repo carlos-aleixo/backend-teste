e5ed53fe0a5a34f32a0862435283905b778bea7c
carloxo307 <carlos.aleixo@rede.ulbra.br>
2023-10-11 17:59:35 -0300

    "Atualizando pra pokedex"

--- app.js
+++ app.js
@@ -1,31 +1,76 @@
 const express = require('express');
-const db=require('./db')
+const db = require('./db');
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/ola', (req,res)=>{res.json({mensagem:'olá mundo'})})
-
-app.get('/tarefas', (req,res)=>{
-    db.all('select * from tarefas',(erro,tarefas)=>{
-        if (erro!=null){
-            console.error(erro); 
-            res.status(500).json({mensagem: 'erro no servidor'})
+app.get('/pokemon/:id', (req, res) => {
+    const id = req.params.id;
+    db.get('SELECT * FROM pokemon WHERE id = ?', [id], (erro, pokemon) => {
+        if (erro) {
+            console.error(erro);
+            return res.status(500).json({ mensagem: 'Erro no servidor' });
         }
-        else{res.json(tarefas)}
-    })
-      
-});
-
-app.post('/tarefas', (req, res) => {
-    const nome = req.body.nome;
-    db.run('insert into tarefas (nome) values (?)', [nome], (erro) => {
-        if (erro != null) {
-            res.status(500).json({mensagem: 'Deu erro'});
-        } else {
-            res.status(201).json({nome: nome})
-        };
+        if (!pokemon) {
+            return res.status(404).json({ mensagem: 'Pokémon não encontrado' });
+        }
+        res.json(pokemon);
     });
 });
 
-app.listen(3030, () => {console.log('servidor executando em localhost:3030')});
+app.get('/pokemon', (req, res) => {
+    db.all('select * from pokemon', (erro, pokemon) => {
+        if (erro) {
+            console.error(erro);
+            return res.status(500).json({ mensagem: 'Erro no servidor' });
+        }
+        res.json(pokemon);
+    });
+});
+
+app.post('/pokemon', (req, res) => {
+    const { nome, tipo } = req.body;
+    db.run('insert into pokemon (nome, tipo) values (?, ?)', [nome, tipo], function(erro) {
+        if (erro) {
+            console.error(erro);
+            return res.status(500).json({ mensagem: 'Erro ao inserir' });
+        }
+        res.status(201).json({ nome, tipo });
+    });
+});
+
+app.delete('/pokemon/:id', (req, res) => {
+    const id = req.params.id;
+    db.run('DELETE FROM pokemon WHERE id = ?', [id], function(erro) {
+        if (erro) {
+            console.error(erro);
+            return res.status(500).json({ mensagem: 'Erro ao deletar' });
+        }
+        if (this.changes === 0) {
+            return res.status(404).json({ mensagem: 'Pokémon não encontrado' });
+        }
+        res.status(200).json({ mensagem: 'Pokémon deletado com sucesso' });
+    });
+});
+
+app.put('/pokemon/:id', (req, res) => {
+    const id = req.params.id;
+    const { nome, tipo } = req.body;
+    
+    db.run('UPDATE pokemon SET nome = ?, tipo = ? WHERE id = ?', [nome, tipo, id], function(erro) {
+        if (erro) {
+            console.error(erro);
+            return res.status(500).json({ mensagem: 'Erro ao atualizar' });
+        }
+        if (this.changes === 0) {
+            return res.status(404).json({ mensagem: 'Pokémon não encontrado' });
+        }
+        res.status(200).json({ mensagem: 'Pokémon atualizado com sucesso' });
+    });
+});
+
+app.listen(3030, () => {
+    console.log('servidor executando em localhost:3030');
+});
+
+"CORS"
--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const sqlite3=require('sqlite3').verbose();
-const db=new sqlite3.Database('./banco.db');
+const db=new sqlite3.Database('./pokemon.db');
 
 db.serialize(()=>{
-    db.run(`create table if not exists tarefas(
+    db.run(`create table if not exists pokemon(
         id integer primary key autoincrement,
-        nome varchar(100)
+        nome varchar(100),
+        tipo varchar(100)
         )`)
 })
 
